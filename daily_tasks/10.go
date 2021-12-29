76db2cbefb2ec2e3ef23b60fa0b5396ee2af3ac9
awaltlov <awaltlov@redhat.com>
2021-12-29 17:50:01 +0100

    Day 10 part 1

--- daily_tasks/10.go
+++ daily_tasks/10.go
@@ -7,8 +7,45 @@ import (
 	"fmt"
 )
 
+var CLOSING_BRACKETS_MAP map[string]string = map[string]string{
+	")": "(",
+	"}": "{",
+	"]": "[",
+	">": "<",
+}
+
+var SCORE map[string]int = map[string]int{
+	")": 3,
+	"]": 57,
+	"}": 1197,
+	">": 25137,
+}
+
+func validate_chunks(chunks string) int {
+	stack := []string{}
+	var score int
+	for _, runeChar := range chunks {
+		closing := string(runeChar)
+		if opening, ok := CLOSING_BRACKETS_MAP[closing]; ok {
+			stackLastIndex := len(stack) - 1
+			if stack[stackLastIndex] != opening {
+				score = SCORE[closing]
+				break
+			}
+			stack = stack[:stackLastIndex]
+		} else {
+			stack = append(stack, closing)
+		}
+	}
+	return score
+}
+
 func part1(lines []string) int {
-	return 1
+	var score int = 0
+	for _, chunks := range lines {
+		score += validate_chunks(chunks)
+	}
+	return score
 }
 
 func part2(lines []string) int {
@@ -19,5 +56,5 @@ func main() {
 	lines := getInputLines("data/10.txt")
 
 	fmt.Println(part1(lines))
-	fmt.Println(part2(lines))
+	// fmt.Println(part2(lines))
 }
