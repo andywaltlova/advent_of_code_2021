b31853fb8e10ef09e5c9fc2231a1f0a88e2ab838
awaltlov <awaltlov@redhat.com>
2021-12-29 23:33:44 +0100

    Day 13

--- daily_tasks/13.go
+++ daily_tasks/13.go
@@ -10,15 +10,71 @@ import (
 // e.g `go run 13.go utils.go`
 
 func part1(thermalMap thermalImaging, folds []fold) int {
-	return 1
+	newThermalMap := thermalImaging{}
+	if folds[0].isHorizontal() {
+		newThermalMap = thermalMap.horizontalFold(folds[0].value)
+	} else {
+		newThermalMap = thermalMap.verticalFold(folds[0].value)
+	}
+	return len(newThermalMap.coordinates)
 }
 
-func part2(thermalMap thermalImaging, folds []fold) int {
-	return 2
+func part2(thermalMap thermalImaging, folds []fold) {
+	for _, f := range folds {
+		if f.isHorizontal() {
+			thermalMap = thermalMap.horizontalFold(f.value)
+		} else {
+			thermalMap = thermalMap.verticalFold(f.value)
+		}
+	}
+
+	for y := 0; y < 6; y++ {
+		for x := 0; x < 39; x++ {
+			c := coordinate{x: x, y: y}
+			if _, ok := thermalMap.coordinates[c]; ok {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println("")
+	}
 }
 
 type thermalImaging struct {
 	coordinates map[coordinate]bool
+}
+
+func (t thermalImaging) horizontalFold(line int) thermalImaging {
+	// y coordinate is folded up
+	// e.g folding by line 7 will fold 8 line on 6 line, 9 line on 5 line ...
+	newCoordinates := make(map[coordinate]bool)
+	for coord := range t.coordinates {
+		if coord.y <= line {
+			newCoordinates[coordinate{x: coord.x, y: coord.y}] = true
+			continue
+		}
+		newY := 2*line - coord.y
+		newCoordinates[coordinate{x: coord.x, y: newY}] = true
+	}
+	newImaging := thermalImaging{coordinates: newCoordinates}
+	return newImaging
+}
+
+func (t thermalImaging) verticalFold(col int) thermalImaging {
+	// x coordinate is folded to the left
+	// e.g folding by col 5 will fold 6 col on 4 col, 9 col on 1 col ...
+	newCoordinates := make(map[coordinate]bool)
+	for coord := range t.coordinates {
+		if coord.x <= col {
+			newCoordinates[coordinate{x: coord.x, y: coord.y}] = true
+			continue
+		}
+		newX := 2*col - coord.x
+		newCoordinates[coordinate{x: newX, y: coord.y}] = true
+	}
+	newImaging := thermalImaging{coordinates: newCoordinates}
+	return newImaging
 }
 
 type coordinate struct {
@@ -29,6 +85,10 @@ type coordinate struct {
 type fold struct {
 	axis  string
 	value int
+}
+
+func (f fold) isHorizontal() bool {
+	return f.axis == "y"
 }
 
 func loadThermalImagingAndFolds(lines []string) (thermalImaging, []fold) {
@@ -61,5 +121,5 @@ func main() {
 	imaging, folds := loadThermalImagingAndFolds(lines)
 
 	fmt.Println(part1(imaging, folds))
-	fmt.Println(part2(imaging, folds))
+	part2(imaging, folds)
 }
