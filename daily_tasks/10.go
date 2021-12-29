ac34f3ffd1ed09416ac0f5cf51754e47cfb1b168
awaltlov <awaltlov@redhat.com>
2021-12-29 18:02:13 +0100

    Day 10 modify and add part 2

--- daily_tasks/10.go
+++ daily_tasks/10.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 var CLOSING_BRACKETS_MAP map[string]string = map[string]string{
@@ -14,14 +15,21 @@ var CLOSING_BRACKETS_MAP map[string]string = map[string]string{
 	">": "<",
 }
 
-var SCORE map[string]int = map[string]int{
+var CLOSING_SCORE map[string]int = map[string]int{
 	")": 3,
 	"]": 57,
 	"}": 1197,
 	">": 25137,
 }
 
-func validate_chunks(chunks string) int {
+var OPENING_SCORE map[string]int = map[string]int{
+	"(": 1,
+	"[": 2,
+	"{": 3,
+	"<": 4,
+}
+
+func validate_chunks(chunks string) (int, []string) {
 	stack := []string{}
 	var score int
 	for _, runeChar := range chunks {
@@ -29,7 +37,7 @@ func validate_chunks(chunks string) int {
 		if opening, ok := CLOSING_BRACKETS_MAP[closing]; ok {
 			stackLastIndex := len(stack) - 1
 			if stack[stackLastIndex] != opening {
-				score = SCORE[closing]
+				score = CLOSING_SCORE[closing]
 				break
 			}
 			stack = stack[:stackLastIndex]
@@ -37,24 +45,40 @@ func validate_chunks(chunks string) int {
 			stack = append(stack, closing)
 		}
 	}
-	return score
+	return score, stack
 }
 
 func part1(lines []string) int {
-	var score int = 0
+	var total_score int = 0
 	for _, chunks := range lines {
-		score += validate_chunks(chunks)
+		score, _ := validate_chunks(chunks)
+		total_score += score
 	}
-	return score
+	return total_score
 }
 
 func part2(lines []string) int {
-	return 2
+	scores := []int{}
+	for _, chunks := range lines {
+		score, stack := validate_chunks(chunks)
+		if score != 0 {
+			continue
+		}
+
+		for i := len(stack) - 1; i >= 0; i-- {
+			symbolScore := OPENING_SCORE[stack[i]]
+			score *= 5
+			score += symbolScore
+		}
+		scores = append(scores, score)
+	}
+	sort.Slice(scores, func(i, j int) bool { return scores[i] < scores[j] })
+	return scores[len(scores)/2]
 }
 
 func main() {
 	lines := getInputLines("data/10.txt")
 
-	fmt.Println(part1(lines))
-	// fmt.Println(part2(lines))
+	fmt.Println(part1(lines)) //316851
+	fmt.Println(part2(lines)) //2182912364
 }
